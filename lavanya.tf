9f691fc4bc531b87a454f271fde2a01df08a1a08
GAGAsree <101580021+GAGAsree@users.noreply.github.com>
2022-03-15 15:13:03 +0530

    Update lavanya.tf

--- lavanya.tf
+++ lavanya.tf
@@ -1,13 +1,13 @@
 provided "aws"{
-region = "us-east"
-access_key = ""
-secret_key = ""
+region = "us-east-1"
+access_key = "AKIAVMFWVN35PU6A6M4R"
+secret_key = "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"
 }
 resource "aws_instance" "aws"{
-ami = "ami-00f7e5c52cof437"
+ami = " ami-0e1d30f2c40c4c701"
 instance_type = "t2.micro"
-availability_zone = "us-east-1"
-key_name = 'suu"
+availability_zone = "us-east-1a"
+key_name = "mysuu"
 } 
 resource "aws_s3_bucket" "abc"{
 bucket = "infosysflipakrt789"

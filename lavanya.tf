3c0885f489f3f2ee9a298bbe3062ddb77a33bbd3
subhashini <subhashini485@gmail.com>
2022-03-15 09:31:50 +0000

    this is first commit

--- lavanya.tf
+++ lavanya.tf
@@ -3,6 +3,12 @@ region = "us-east"
 access_key = ""
 secret_key = ""
 }
+resource "aws_instance" "aws"{
+ami = "ami-00f7e5c52cof437"
+instance_type = "t2.micro"
+availability_zone = "us-east-1"
+key_name = 'suu"
+} 
 resource "aws_s3_bucket" "abc"{
 bucket = "infosysflipakrt789"
 acl = "private"
